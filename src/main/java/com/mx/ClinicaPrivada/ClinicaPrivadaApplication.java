5bd2a4609e71cf535bb8cd0c04285c4db6bca171
Zerch Dev <sergiog19zzh@gmail.com>
2025-01-17 13:24:29 -0600

    integracion del proyecto con postgres

---
+++ src/main/java/com/mx/ClinicaPrivada/ClinicaPrivadaApplication.java
@@ -0,0 +1,13 @@
+package com.mx.ClinicaPrivada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaPrivadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaPrivadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/ClinicaPrivada/ClinicaPrivadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.ClinicaPrivada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaPrivadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
